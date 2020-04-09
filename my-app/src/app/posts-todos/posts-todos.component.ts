f18fe75855aa56dda387deccab470ba1780f7fc9
anael <anael.mashinsky@gmail.com>
2020-04-09 21:20:45 +0300

    nested routes with session storage data

--- my-app/src/app/posts-todos/posts-todos.component.ts
+++ my-app/src/app/posts-todos/posts-todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-posts-todos',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts-todos.component.css']
 })
 export class PostsTodosComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  constructor(private ar: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-  }
+    this.ar.params.subscribe(data => {
+      this.id = data.id;
+      sessionStorage['id'] = this.id;
+      console.log('from post-todos');
+      console.log(sessionStorage['id']);
 
+      this.router.navigate(
+        [{ outlets: { primary: ['todos', this.id], posts: 'posts' } }],
+        { relativeTo: this.ar }
+      );
+    });
+  }
 }
